3a15d105ce3a3853efae67ea2f8974459da11f7c
jps <jpsalo@zoho.com>
2020-07-17 20:22:56 +0300

    Added spaces sample

---
+++ src/app/components/spaces/spaces.stories.ts
@@ -0,0 +1,10 @@
+import { SpacesComponent } from './spaces.component';
+
+export default {
+  title: 'Spaces',
+  component: SpacesComponent,
+};
+
+export const Text = () => ({
+  component: SpacesComponent,
+});
