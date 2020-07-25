42ca97869ac89260499a5037da31095e8c11b6dd
jps <jpsalo@zoho.com>
2020-07-25 17:54:57 +0300

    Beginnings of Contentful integration

---
+++ src/app/services/contentful.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+// import { createClient, Entry } from 'contentful';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContentfulService {
+  constructor() {}
+}
